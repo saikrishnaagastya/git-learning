06920fe4e8bd2326f7cf36b8f87a42aaba963616
zindal <saikrishna.araju@gmail.com>
2021-02-17 11:47:58 +0530

    Updated the first git project

---
+++ Oridey/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Oridey.Startup))]
+
+namespace Oridey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
